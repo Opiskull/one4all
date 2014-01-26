c7975bf4e0202c2e0db3782924ded9e6a33a372d
Christian.Onuk@gmx.net <Christian.Onuk@gmx.net>
2014-01-27 00:06:43 +0100

    Fixed LogoutButton

--- client/src/app/auth/auth.js
+++ client/src/app/auth/auth.js
@@ -55,6 +55,7 @@ angular.module('auth', ['ngRoute'])
             $http.post('/api/auth/logout')
                 .success(function (data) {
                     localStorage.removeItem('access_token');
+                    authInfo = {};
                     Restangular.setDefaultHeaders({'Authorization':''});
                     $location.url('/');
                 });
