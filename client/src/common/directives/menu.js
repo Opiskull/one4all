6e7960e5315e1845fb282f17c6a61815f8f8efba
Christian.Onuk@gmx.net <Christian.Onuk@gmx.net>
2014-03-03 18:55:25 +0100

    Refactored Menu Fixed double line

---
+++ client/src/common/directives/menu.js
@@ -0,0 +1,45 @@
+angular.module('14all').directive('menu',function(){
+    return {
+        restrict: 'E',
+        scope:{},
+        transclude:true,
+        replace: true,
+        template : '<ul class="nav navbar-nav" ng-transclude></ul>',
+        controller:function($scope,$element,$attrs,$transclude){
+            $scope.items = [];
+
+            this.addItem = function(item){
+                $scope.items.push(item);
+            };
+
+            $scope.$on("$routeChangeSuccess",function(event,current,previous){
+                var currentPath = '';
+                if(current.$$route){
+                    currentPath = current.$$route.originalPath;
+                }
+                angular.forEach($scope.items,function(item,key){
+                    item.active = item.strippedPath === currentPath;
+                });
+            });
+        }
+    }
+}).directive('menuItem',function(){
+    return {
+        priority: 300,
+        require: "^menu",
+        template:'<li ng-class="{ active: active }"><a href="{{path}}">{{displayTitle}}</a></li>',
+        restrict: 'E',
+        replace:true,
+        scope:{
+            displayTitle: '@',
+            path: '@'
+        },
+        link : function($scope,$element,$attrs,menuController){
+            menuController.addItem($scope);
+            $scope.active = false;
+            if($scope.path){
+                $scope.strippedPath = $scope.path.substring(1);
+            }
+        }
+    };
+});
