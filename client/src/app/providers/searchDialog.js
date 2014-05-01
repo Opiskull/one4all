8081b3fba5c8baab114b8ef33826a8b179e5dc24
Christian.Onuk@gmx.net <Christian.Onuk@gmx.net>
2014-05-01 19:38:24 +0200

    Added Encoding to search Add provider to info

--- client/src/app/providers/searchDialog.js
+++ client/src/app/providers/searchDialog.js
@@ -1,39 +1,48 @@
-angular.module('14all.providers').controller('searchDialogCtrl', ['$scope', '$modalInstance', 'initialtitle', 'initialprovider', 'searchService', function ($scope, $modalInstance, initialtitle, initialprovider, searchService) {
-    $scope.selectedInfo = {};
-    $scope.infos = [];
-    $scope.selectedTitle = '';
-    $scope.keyword = initialtitle;
-    $scope.currentProvider = searchService.getProvider(initialprovider);
-    $scope.providers = searchService.getProviders();
+angular.module('14all.providers')
+    .controller('searchDialogCtrl', ['$scope', '$modalInstance', 'initialtitle', 'initialprovider', 'searchService', function ($scope, $modalInstance, initialtitle, initialprovider, searchService) {
+        $scope.selectedInfo = {};
+        $scope.infos = [];
+        $scope.selectedTitle = '';
+        $scope.keyword = initialtitle;
+        $scope.currentProvider = searchService.getProvider(initialprovider);
+        $scope.providers = searchService.getProviders();
 
-    $scope.search = function (keywords) {
-        return $scope.currentProvider.search(keywords).then(function (items) {
-            $scope.infos = items;
-        });
-    };
+        $scope.search = function (keywords) {
+            var encodedKeywords = encodeURIComponent(keywords);
+            return $scope.currentProvider.search(encodedKeywords).then(function (items) {
+                $scope.infos = items;
+            });
+        };
 
-    $scope.selectProvider = function (provider) {
-        $scope.currentProvider = provider;
-    };
+        $scope.selectProvider = function (provider) {
+            $scope.currentProvider = provider;
+            $scope.infos = [];
+            $scope.selectedInfo = {};
+            $scope.selectedTitle = '';
+        };
 
-    $scope.selectInfo = function (info) {
-        $scope.selectedInfo = info;
-        $scope.selectedTitle = info.title;
-    };
+        $scope.selectInfo = function (info) {
+            $scope.selectedInfo = info;
+            $scope.selectedTitle = info.title;
+        };
 
-    $scope.selectTitle = function (title) {
-        $scope.selectedTitle = title;
-    };
+        $scope.selectTitle = function (title) {
+            $scope.selectedTitle = title;
+        };
 
-    $scope.ok = function () {
-        var result = {title: $scope.selectedTitle, info: $scope.selectedInfo};
-        $modalInstance.close(result);
-    };
+        $scope.ok = function () {
+            var result = {
+                title: $scope.selectedTitle,
+                info: $scope.selectedInfo
+            };
+            result.info.provider = $scope.currentProvider.title;
+            $modalInstance.close(result);
+        };
 
-    $scope.cancel = function () {
-        $modalInstance.dismiss();
-    };
-}])
+        $scope.cancel = function () {
+            $modalInstance.dismiss();
+        };
+    }])
     .factory('searchDialogService', ['$modal', function ($modal) {
         var search = function (title, initalprovider) {
             return $modal.open({
