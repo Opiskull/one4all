c92108045d9a348e17f4e849093ec062f057ce39
Christian.Onuk@gmx.net <Christian.Onuk@gmx.net>
2014-05-04 02:47:19 +0200

    added info directive show provider details

--- client/src/app/providers/info.js
+++ client/src/app/providers/info.js
@@ -1,7 +1,15 @@
-angular.module('providers')
-    .factory('infoService',[function(){
-        var service = {
-
-        };
-        return service;
-    }]);
+angular.module('providers').directive('info',['searchService',function(searchService) {
+    return {
+        restrict:'E',
+        templateUrl:'providers/info.html',
+        link: function(scope, element, attrs) {
+            if(scope.info) {
+                var provider = searchService.getProvider(scope.info.provider)
+                scope.detailTemplate = provider.detailUrl;
+            }
+        },
+        scope:{
+            info:'='
+        }
+    };
+}]);
