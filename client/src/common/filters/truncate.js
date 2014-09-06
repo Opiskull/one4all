452b14ecc0096e5d414b5d9640035315dc0cc4a6
Christian.Onuk@gmx.net <Christian.Onuk@gmx.net>
2014-09-06 03:08:58 +0200

    fix(truncate-filter): error when end not set

--- client/src/common/filters/truncate.js
+++ client/src/common/filters/truncate.js
@@ -2,6 +2,8 @@ angular.module('one4all').filter('truncate', function () {
     return function (text, length, end, open) {
         if (!open) {
             if (text) {
+                if(end === undefined)
+                    end = '';
                 if (text.length >= length) {
                     return text.substring(0, length - end.length) + end;
                 }
