9ad39d1bfeec0f5c0bffef3a2701551776e351bc
Christian.Onuk@gmx.net <Christian.Onuk@gmx.net>
2014-10-21 23:34:04 +0200

    fix(info): fix for virtuals

--- server/lib/mongoose-plugins.js
+++ server/lib/mongoose-plugins.js
@@ -58,13 +58,15 @@ function changesPlugin(schema){
 }
 
 function hiddenPlugin(schema) {
+    schema.set('toObject', {virtuals: true});
     schema.set('toJSON', {
         transform: function (doc, ret, options) {
             ret.id = ret._id;
             delete ret._id;
             delete ret.__v;
             return ret;
-        }
+        },
+        virtuals: true
     });
 }
 
